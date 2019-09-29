c0abe10533c089c29452538bfc71c37ed8249ad4
rajat-008 <mailrajat008@gmail.com>
2019-09-30 00:19:08 +0530

    updated the create test page

--- webapp/static/webapp/scripts.js
+++ webapp/static/webapp/scripts.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded',()=>{
   var count=0;
+
   document.getElementById('add_question').onclick=()=>{
-    count+=1;
-    document.getElementById('questions').innerHTML+=`<li>Question no ${count}</li>`;
+    if(count>10){
+      alert("Only 10 questions allowed");
+      return false;
+    }
+    question=document.getElementById('question').value;
+    a=document.getElementById('option-a').value;
+    b=document.getElementById('option-b').value;
+    c=document.getElementById('option-c').value;
+    d=document.getElementById('option-d').value;
+    document.getElementById('questions').innerHTML+=`<li><h5> ${question}</h5><ol type="A"><li>${a}</li><li>${b}</li><li>${c}</li><li>${d}</li></ol></li>`;
+    count++;
+
   };
 
 });
