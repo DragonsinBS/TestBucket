f7c26032bc0f68b385fe1455dc9ce6fee3729cec
rajat-008 <mailrajat008@gmail.com>
2019-12-05 21:11:15 +0530

    added documentation tool

--- testbucket/settings.py
+++ testbucket/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['192.168.43.191','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admindocs',
     'webapp.apps.WebappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
